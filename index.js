cc5a6125108329f080a1ff62bd4b2be48fd0d5d3
DanyCarter <danielcadenas88@gmail.com>
2023-10-25 08:46:58 +0200

    tenemos las firmas o signaturas de los metodos

---
+++ index.js
@@ -0,0 +1,29 @@
+let saldo = 1000
+const PIN_CORRECTO = "1234"
+let intentosRestantes = 3
+
+function mostrarSaldo(){
+
+}
+
+function depositar() {
+
+}
+
+function retirar(){
+
+}
+
+function transferir(){
+
+}
+
+function iniciarSesion(){
+
+}
+
+function operacionesCajero(){
+
+}
+
+i
